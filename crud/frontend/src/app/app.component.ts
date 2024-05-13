d0d73131a195e7f000a0e43199a15411bf48c8cd
Michelle Pani <michelle.pani@shiftsistemas.com.br>
2024-05-13 11:10:30 -0300

    Estilização do header

--- crud/frontend/src/app/app.component.ts
+++ crud/frontend/src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatChipsModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
     <app-header></app-header>
+    <h1>Welcome to {{course}}!</h1>
+    <router-outlet />
+    <div>
+    <mat-chip-listbox aria-label="Fish selection" class="p-2">
+      <mat-chip-option>Opção 1</mat-chip-option>
+      <mat-chip-option>Opção 2</mat-chip-option>
+      <mat-chip-option>Opção 3</mat-chip-option>
+      <mat-chip-option>Opção 4</mat-chip-option>
+    </mat-chip-listbox>
+    </div>
+
+    
   `,
-  styles: [],
+  styles: ['body { padding: 20px; }'],
 })
 export class AppComponent {
-  title = 'frontend';
+  course = 'Angular course';
 }
